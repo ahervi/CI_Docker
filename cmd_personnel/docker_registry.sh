fbb754b25c52ec0054d0c2af99f18b6e9dbd047b
Antoine hervieu <hervieu.antoine@gmail.com>
2019-03-14 21:58:38 +0100

    Nettoyage de presque printemps

--- cmd_personnel/docker_registry.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#commande à exécuter pour push une image sur le registre.
-docker pull debian
-docker tag debian 192.168.2.4:5000/debian
-docker push 192.168.2.4:5000/debian
--- cmd_personnel/git_clone.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#Commandes à exécuter pour git clone du serveur gitlab.
-export GIT_SSL_NO_VERIFY=true
-git clone https://192.168.2.3/hlk/test.git
